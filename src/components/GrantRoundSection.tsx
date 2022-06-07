1cedf939f817ce6b4cc76ed3fa5998c0c2614ea3
Gail Wilson <gail.de.wilson@gmail.com>
2022-06-07 18:30:18 -0400

    Add round empty state & collapsable round

--- src/components/GrantRoundSection.tsx
+++ src/components/GrantRoundSection.tsx
@@ -1,8 +1,10 @@
-import { Image, Text, Box, Button, Flex } from '@chakra-ui/react';
+import { Image, Text, Box, Button, Flex, Collapse } from '@chakra-ui/react';
 import { ethers } from 'ethers';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import boltSrc from '../assets/bolt.svg';
+import checkmarkWhiteSrc from '../assets/checkmark_white.svg';
+import proposalSrc from '../assets/proposal.svg';
 import GrantProposalCard from './GrantProposalCard';
 import ProgressBar from './ProgressBar';
 
@@ -43,6 +45,7 @@ export type GrantRoundSectionProps = {
 };
 
 function GrantRoundSection({ round, inProgress }: GrantRoundSectionProps) {
+  const [expandProposals, setExpandProposals] = useState(false);
   const allocationAmount = round.tokenAllocationAmount ? ethers.utils.formatEther(round.tokenAllocationAmount) : 0;
   // TODO: get proposals from rounds, for now stub it
   const proposals = [
@@ -55,6 +58,10 @@ function GrantRoundSection({ round, inProgress }: GrantRoundSectionProps) {
   const onPressSubmitProposal = useCallback(() => {
     // TODO: add interaction for submitting a new proposal
   }, []);
+
+  const onToggleExpandProposals = useCallback(() => {
+    setExpandProposals(!expandProposals);
+  }, [expandProposals]);
 
   return (
     <Box width="100%" padding="60px" background={inProgress ? 'purple-medium' : 'purple-light'} borderRadius="20px">
@@ -72,10 +79,10 @@ function GrantRoundSection({ round, inProgress }: GrantRoundSectionProps) {
             height="20px"
             marginRight="6px"
             marginLeft="6px"
-            background="secondary-green"
+            background={inProgress ? 'secondary-green' : 'primary-purple'}
             borderRadius={40}
           >
-            <Image height="12px" src={boltSrc} />
+            {inProgress ? <Image height="12px" src={boltSrc} /> : <Image height="9px" src={checkmarkWhiteSrc} />}
           </Box>
 
           <Text marginEnd="6px" fontSize="sm" fontWeight="bold">
@@ -84,30 +91,44 @@ function GrantRoundSection({ round, inProgress }: GrantRoundSectionProps) {
           <Text fontSize="sm"> for a max of {round.maxWinnerCount} voted projects</Text>
         </Flex>
 
-        {inProgress && <Button onClick={onPressSubmitProposal}>Submit Proposal</Button>}
+        {inProgress ? (
+          <Button onClick={onPressSubmitProposal}>Submit Proposal</Button>
+        ) : (
+          <Button onClick={onToggleExpandProposals} variant="link">
+            {expandProposals ? 'Show less' : 'Show proposals'}
+          </Button>
+        )}
       </Flex>
 
-      {!inProgress && (
+      {inProgress ? (
+        <Box width="100%" paddingTop="42px">
+          <ProgressBar progressNumber={368.81} totalNumber={958} />
+        </Box>
+      ) : (
         <Flex gap="6px" paddingTop="16px">
           <Text fontSize="sm" fontWeight="bold">
             Issued
           </Text>
           {/* TODO: put in real date */}
-          <Text fontSize="sm">May 10</Text>{' '}
+          <Text fontSize="sm">May 10</Text>
         </Flex>
       )}
 
-      {inProgress && (
-        <Box width="100%" paddingTop="42px">
-          <ProgressBar progressNumber={368.81} totalNumber={958} />
-        </Box>
-      )}
+      <Collapse in={inProgress || expandProposals}>
+        <Flex flexWrap="wrap" gap="24px" paddingTop="42px">
+          {proposals.map(p => (
+            <GrantProposalCard key={p.id} proposal={p} inProgress={inProgress} />
+          ))}
+        </Flex>
+      </Collapse>
 
-      <Flex flexWrap="wrap" gap="24px" paddingTop="42px">
-        {proposals.map(p => (
-          <GrantProposalCard key={p.id} proposal={p} inProgress={inProgress} />
-        ))}
-      </Flex>
+      {inProgress && proposals.length === 0 && (
+        <Flex alignItems="center" flexDirection="column" gap="24px">
+          <Image height="161px" src={proposalSrc} />
+          <Text>Be the first to add a proposal</Text>
+          <Button onClick={onPressSubmitProposal}>Submit Proposal</Button>
+        </Flex>
+      )}
     </Box>
   );
 }
