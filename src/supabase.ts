9b0c137adfa9b6b417380fcdafea829dc44d59a3
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-07-12 11:32:53 -0400

    use env vars

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'http://localhost:54321';
+const SUPABASE_URL = import.meta.env.SUPABASE_URL ?? 'http://localhost:54321';
 const SUPABASE_ANON_KEY =
+  import.meta.env.SUPABASE_ANON_KEY ??
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs';
 
 export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
