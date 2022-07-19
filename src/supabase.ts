634b0bb7546c3b0b3e55a803cb9a5ae15fad368e
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-07-19 17:41:21 -0400

    fix functions url

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL ?? 'http://localhost:54321';
+const SUPABASE_FUNCTIONS_URL = import.meta.env.VITE_SUPABASE_FUNCTIONS_URL ?? 'http://localhost:54321/functions/v1';
 const SUPABASE_ANON_KEY =
   import.meta.env.VITE_SUPABASE_ANON_KEY ??
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs';
@@ -9,7 +10,7 @@ export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const functionRequest = (name: string, data?: any) => {
-  return fetch(import.meta.env.VITE_SUPABASE_URL ? `${SUPABASE_URL}/rpc` : `${SUPABASE_URL}/functions/v1/rpc`, {
+  return fetch(`${SUPABASE_FUNCTIONS_URL}/rpc`, {
     method: 'POST',
     body: JSON.stringify({ method: name, ...data }),
     headers: {
